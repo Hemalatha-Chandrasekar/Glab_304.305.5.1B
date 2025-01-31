e4f3839e7f92289f99123e561665eafe01858333
Hemalatha-Chandrasekar <hema.alliswell@gmail.com>
2025-01-30 21:27:41 -0600

    Initial commit

--- src/main/java/com/hema/App.java
+++ src/main/java/com/hema/App.java
@@ -1,17 +1,83 @@
 package com.hema;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import model.Department;
+import model.Teacher;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        oneToMany();
+        manyToOne();
+    }
+
+    public static void manyToOne() {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        //creating departments
+        Department dept1 = new Department("IT");
+        Department dept2 = new Department("HR");
+
+        //creating teacher
+        Teacher t1 = new Teacher(1,"1000", "MHaseeb");
+        Teacher t2 = new Teacher(2,"2220","Shahparan");
+        Teacher t3 = new Teacher(3,"3000","James");
+        Teacher t4 = new Teacher(4,"40000", "Joseph");
+
+        //Storing Departments in database
+        session.persist(dept1);
+        session.persist(dept2);
+        //Storing teachers in database
+        session.persist(t1);
+        session.persist(t2);
+        session.persist(t3);
+        session.persist(t4);
+        transaction.commit();
+    }
+    public static void oneToMany(){
+        SessionFactory factory = new
+                Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction t = session.beginTransaction();
+
+        //Creating Department Object
+        Department department = new Department("Development");
+
+        //creating teacher with department
+        Teacher t1 = new Teacher("1000","MHaseeb", department);
+        Teacher t2 = new Teacher("2220","Shahparan", department);
+        Teacher t3 = new Teacher("3000","James", department);
+        Teacher t4 = new Teacher("40000","Joseph", department);
+        Teacher t5 = new Teacher("200","Ali", department);
+
+        //Add teacher entity object to the list
+        ArrayList<Teacher> teachersList = new ArrayList<>();
+        teachersList.add(t1);
+        teachersList.add(t2);
+        teachersList.add(t3);
+        teachersList.add(t4);
+        teachersList.add(t5);
+
+
+        department.setTeacherList(teachersList);
+
+        //Storing Department
+        session.persist(department);
+
+        //persist teachers
+        session.persist(t1);
+        session.persist(t2);
+        session.persist(t3);
+        session.persist(t4);
+        session.persist(t5);
+
+
+        t.commit();
     }
 }
--- src/main/java/model/Teacher.java
+++ src/main/java/model/Teacher.java
@@ -1,4 +1,66 @@
 package model;
 
-public class Teacher {
+import jakarta.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table
+public class Teacher implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int teacherId;
+    private String salary;
+    private String teacherName;
+
+    @ManyToOne
+    private Department department;
+
+    public Teacher(int teacherId, String salary, String teacherName) {
+        super();
+        this.teacherId = teacherId;
+        this.salary = salary;
+        this.teacherName = teacherName;
+    }
+    public Teacher() {}
+
+    public Teacher(String s, String mHaseeb, Department dept1) {
+    }
+
+    public Teacher(String number, String mHaseeb) {
+    }
+
+    public Department getDep() {
+        return department;
+    }
+
+    public void setDep(Department department) {
+        this.department = department;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
 }
